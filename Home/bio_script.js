a8deb18a791c1817598f052b02c2b98c2621131b
TheoBueno <143668478+TheoBueno@users.noreply.github.com>
2023-10-18 23:39:05 -0400

    alterations to Bio, Timeline and Index

---
+++ Home/bio_script.js
@@ -0,0 +1,27 @@
+
+/* ACCORDION EXTRA SECTION
+–––––––––––––––––––––––––––––––––––––––––––––––––– */
+
+//  $(function () {
+//    $('[data-toggle="tooltip"]').tooltip({
+//      delay: { show: 800, hide: 500 },
+//      trigger: "hover"
+//    });
+//  });
+
+
+/* ACCORDION SECTION
+–––––––––––––––––––––––––––––––––––––––––––––––––– */
+
+const choiceArray = document.querySelectorAll(".choice")
+
+choiceArray.forEach((card) => {
+    card.addEventListener("click", () => {
+        choiceArray.forEach((element) => {
+            element.classList.remove("expand", "unset")
+            element.classList.add('small')
+        })
+        card.classList.remove("small")  /* Can switch toogle for remove */
+        card.classList.add('expand') /* Can switch toogle for add */
+    });
+});
